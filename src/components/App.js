f7a3a1949e5a5ddc658274adc9c945bd53a58e71
Charles Peace <charlespeace@hotmail.com>
2023-09-26 20:35:23 -0700

    update ui for links

--- src/components/App.js
+++ src/components/App.js
@@ -71,26 +71,25 @@ function App() {
           <button className="dropbtn">about</button>
           <div className="dropdown-content-container">
             {/* Links go here */}
-            <a href='https://knowyourmeme.com/memes/%D1%88%D0%B0%D0%B9%D0%BB%D1%83%D1%88%D0%B0%D0%B9-smurf-cat'><img src={kym} width='80px'/><span>history</span></a>
-            <a href='https://flooz.xyz/trade/how-to-buy/0xfF836A5821E69066c87E268bC51b849FaB94240C'><img src={fl} width='80px'/>how to buy</a>
-            <a href='https://twitter.com/smurfcateth'><img src={tw} width='80px'/><span>memes</span></a>
-            <a href='https://t.me/smurfcateth'><img src={tg} width='80px'/><span>smurf chat</span></a>
+            <a href='https://knowyourmeme.com/memes/%D1%88%D0%B0%D0%B9%D0%BB%D1%83%D1%88%D0%B0%D0%B9-smurf-cat'><img src={kym} width='80px'/>history</a>
+            <a href='https://flooz.xyz/trade/how-to-buy/0xfF836A5821E69066c87E268bC51b849FaB94240C'><img src={fl} width='80px'/>tutorial</a>
+            <a href='https://twitter.com/smurfcateth'><img src={tw} width='80px'/>memes</a>
+            <a href='https://t.me/smurfcateth'><img src={tg} width='80px'/>smurf chat</a>
           </div>
         </div>
         <div className="dropdown">
           <button className="dropbtn">token info</button>
           <div className="dropdown-content-container">
             {/* Links go here */}
-            <a href='https://etherscan.io/token/0xff836a5821e69066c87e268bc51b849fab94240c'><img src={es} width='80px'/><span>etherscan</span></a>
-            <a href='https://www.dextools.io/app/en/ether/pair-explorer/0x977c5fcf7a552d38adcde4f41025956855497c6d'><img src={dx} width='70'/><span>dextools</span></a>
-            <a href='https://coinmarketcap.com/currencies/real-smurf-cat-eth/'><img src={cmc} width='80px'/><span>coinmarketcap</span></a>
-            <a href='https://www.coingecko.com/en/coins/real-smurf-cat'><img src={cg} width='80px'/><span>coingecko</span></a>
+            <a href='https://etherscan.io/token/0xff836a5821e69066c87e268bc51b849fab94240c'><img src={es} width='80px'/>etherscan</a>
+            <a href='https://www.dextools.io/app/en/ether/pair-explorer/0x977c5fcf7a552d38adcde4f41025956855497c6d'><img src={dx} width='70'/>dextools</a>
+            <a href='https://coinmarketcap.com/currencies/real-smurf-cat-eth/'><img src={cmc} width='80px'/>coinmarketcap</a>
+            <a href='https://www.coingecko.com/en/coins/real-smurf-cat'><img src={cg} width='80px'/>coingecko</a>
           </div>
         </div>
-        <div className="dropdown">
+        {/* <div className="dropdown">
           <button className="dropbtn">where to buy</button>
           <div className="dropdown-content-container">
-            {/* Links go here */}
             <a href='#here'><img src={here} width='80px'/>on this site</a>
             <a href='https://app.uniswap.org/swap?inputCurrency=ETH&outputCurrency=0xfF836A5821E69066c87E268bC51b849FaB94240C'><img src={uni} width='80px'/>uniswap</a>
             <a href='https://www.bitget.com/spot/SMURFCATUSDT?type=spot'><img src={bg} width='80px'/>bitget</a>
@@ -98,21 +97,37 @@ function App() {
             <a href='https://ascendex.com/en/cashtrade-spottrading/usdt/smurfcat'><img src={ae} width='100px'/>ascendex</a>
             <a href='https://www.mexc.com/exchange/SMURFCAT_USDT?_from=search_spot_trade'><img src={mx} width='100px'/>mexc</a>
           </div>
-        </div>
+        </div> */}
+      <a href='https://app.uniswap.org/swap?inputCurrency=ETH&outputCurrency=0xfF836A5821E69066c87E268bC51b849FaB94240C'>
+        <button className='uni'>buy on uniswap</button>
+      </a>
       </div>
       <body>
-        <div className='top container front'>
           <a>
             <img src={logo} className="App-logo" alt="logo" width='500vw'/>
           </a>
-        </div>
         <h1 className='top front'>HE IS REAL.</h1>
-      <header className="App-header">
-      </header>
-      <p className='title'><span className='blue'>SMURFCAT</span> IS A MEMECOIN LAUNCHED ON THE ETHEREUM BLOCKCHAIN. THE CRYPTOCURRENCY WAS CREATED AS A TRIBUTE TO THE SMURF CAT INTERNET MEME ORIGINALLY CREATED BY NATE HALLINAN, AND NEWLY POPULARIZED BY TIKTOK.</p>
+        <div className='container'>
+          <a href='https://www.mexc.com/exchange/SMURFCAT_USDT?_from=search_spot_trade' className='item'>
+            <img src={mx} width='62.5px' alt='mexc icon' />
+            <span>MEXC</span>
+          </a>
+          <a href='https://www.bitget.com/spot/SMURFCATUSDT?type=spot' className='item'>
+            <img src={bg} width='40px' alt='bitget icon' />
+            <span>BitGet</span>
+          </a>
+          <a href='https://poloniex.com/trade/SMURFCAT_USDT?type=spot' className='item'>
+            <img src={pl} width='40px' alt='poloniex icon' />
+            <span>Poloniex</span>
+          </a>
+        <a href='https://ascendex.com/en/cashtrade-spottrading/usdt/smurfcat' className='item'>
+          <img src={ae} width='50px' alt='ascendex icon' />
+          <span>AscendEX</span>
+          </a>
+        </div>
+      <p className='title'><span className='blue'>SMURFCAT</span> is a memecoin launched on the ethereum blockchain. he is so cute! this little guy is just going on his jolly way!<br/><br/>the token was created as a tribute to the smurf cat internet meme originally created by nate hallinan, and newly popularized by tiktok.</p>
       <img src={td} width='400px'/>
       <p className='title front'>ZERO TAX, CONTRACT RENOUNCED,<br/>100% OF SUPPLY SENT TO UNISWAP & LP BURNT</p>
-      <iframe id='here' className='swap' frameborder="0" allow="clipboard-read *; clipboard-write *; web-share *; accelerometer *; autoplay *; camera *; gyroscope *; payment *; geolocation *" src="https://flooz.xyz/embed/trade?swapDisabled=false&swapToTokenAddress=0xfF836A5821E69066c87E268bC51b849FaB94240C&swapLockToToken=false&onRampDisabled=false&onRampAsDefault=false&onRampTokenAddress=eth&stakeDisabled=true&network=eth&lightMode=false&primaryColor=%23316bc9&backgroundColor=transparent&roundedCorners=24&padding=20&margin=400" ></iframe>
       <p className='fairy'>concept by nate hallinan 2014, made possible by dmitri</p>
     </body>
   </div>
